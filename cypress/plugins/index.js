9257c7d465609bbb94f9717f0897ac9ce39a557a
eliaset096 <eliaset096@gmail.com>
2021-07-20 17:12:52 -0500

    TypeScrit tests configured

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -20,3 +20,16 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
+
+const wp = require('@cypress/webpack-preprocessor')
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+ module.exports = (on, config) => {
+  const options = {
+    webpackOptions: require('../../webpack.config'),
+  }
+
+  on('file:preprocessor', wp(options))
+}
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    mode: 'development',
+    devtool: 'eval-source-map',
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    module: {
+      rules: [
+        {
+
+          test: /\.ts$/,
+          exclude: [/node_modules/],
+          use: [
+            {
+              loader: 'ts-loader',
+              options: {
+                transpileOnly: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
+  }
+
+  
