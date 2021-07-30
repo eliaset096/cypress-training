894ae1fba8a98255de49c0b7e2f2313f0561d306
eliaset096 <eliaset096@gmail.com>
2021-07-30 10:35:27 -0500

    corrections applied

--- cypress/integration/buy-shirt.spec.ts
+++ cypress/integration/buy-shirt.spec.ts
@@ -1,39 +1,45 @@
 import { AddressStepPage, LoginPage, MenuContentPage, PaymentStepPage, ProductsListPage, ShippingstepPage, ShopingCartPage } from "../page/index"
 
+let menuContentPage : MenuContentPage;
+let productsListPage : ProductsListPage;
+let shopingCartPage : ShopingCartPage;
+let loginPage : LoginPage;
+let addressStepPage : AddressStepPage;
+let shippingstepPage : ShippingstepPage;
+let paymentStepPage : PaymentStepPage;
+let tShirtsNamesArray: string[];
+
 describe("Buy a t-shirt", () => {
 
-  const menuContentPage = new MenuContentPage()
-  const productsListPage = new ProductsListPage()
-  const shopingCartPage = new ShopingCartPage()
-  const loginPage = new LoginPage()
-  const addressStepPage = new AddressStepPage()
-  const shippingstepPage = new ShippingstepPage()
-  const paymentStepPage = new PaymentStepPage()
+  before(() => {
+    tShirtsNamesArray =  ["Faded Short Sleeve T-shirts"];
+    menuContentPage = new MenuContentPage()
+    productsListPage = new ProductsListPage()
+    shopingCartPage = new ShopingCartPage()
+    loginPage = new LoginPage()
+    addressStepPage = new AddressStepPage()
+    shippingstepPage = new ShippingstepPage()
+    paymentStepPage = new PaymentStepPage()
+  })
 
   it("then the t-shirt should be bought", () => {
-
-    const tShirtsNamesArray: string[] = ["Faded Short Sleeve T-shirts"];
-    const sThirtsNumber = 1; 
 
     menuContentPage.visitMenuContentPage()
     menuContentPage.goToTShirtMenu()
     
-    productsListPage.validateItemsNumber(sThirtsNumber)
+    productsListPage.validateItemsNumber(tShirtsNamesArray.length)
     productsListPage.validateItemsNames(tShirtsNamesArray)
+   
     productsListPage.selectTShirtOfProductsList()
-
     shopingCartPage.addTShirtToCart()
     shopingCartPage.proceedTShirtSelectInCart()
-
     loginPage.login("aperdomobo@gmail.com", "WorkshopProtractor")
-
     addressStepPage.confirmAddress()
-
     shippingstepPage.acceptTerms()
     shippingstepPage.proceedCarrier()
-
     paymentStepPage.paybyBankWire()
     paymentStepPage.confirmPay()
+    
     paymentStepPage.getConfirmationTitle().should("have.text", "Your order on My Store is complete.")
 
   });
--- cypress/integration/dresses-list.spec.ts
+++ cypress/integration/dresses-list.spec.ts
@@ -4,22 +4,21 @@ describe("the user navigates to the dresses page should", () => {
 
   let menuContentPage: MenuContentPage;
   let dressesListPage: DressesListPage;
-
+  let dressesNamesArray: string[];
+  
   before(() => {
+    dressesNamesArray = ["Printed Dress", "Printed Dress","Printed Summer Dress", "Printed Summer Dress","Printed Chiffon Dress"];
     menuContentPage = new MenuContentPage();
     dressesListPage = new DressesListPage();
   })
 
   it("show the available dresses", () => {
 
-    const dressesNamesArray: string[] = ["Printed Dress", "Printed Dress","Printed Summer Dress", "Printed Summer Dress","Printed Chiffon Dress"];
-    const dressesNumber = 5; 
-
     menuContentPage.visitMenuContentPage()
     menuContentPage.goToDressesMenu()
-
     dressesListPage.getDressProducts()
-    dressesListPage.validateItemsNumber(dressesNumber)
+
+    dressesListPage.validateItemsNumber(dressesNamesArray.length)
     dressesListPage.validateItemsNames(dressesNamesArray)
     
   })
